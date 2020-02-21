6732717d3cdc5ff5091c218da093e19821e509a9
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-02-21 16:09:21 +0100

    DB version is cleaned

--- Node/server.js
+++ Node/server.js
@@ -3,10 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
 
+// DB stuff
 var mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://dbUser:dbUserPassword@cluster0-j0lfr.azure.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
     .then(() => console.log("DB Connected"))
     .catch(err => console.log(err));
+
+// initialize express middleware for handling POST data.
+// app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const Schema = mongoose.Schema;
 
@@ -16,29 +21,8 @@ var carSchema = new Schema({
     brand: String
 });
 
-
 // Initialize the model.
 const Car = mongoose.model('Car', carSchema);
-
-
-// initialize express middleware for handling POST data.
-// app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-var myCars = {
-    cars: [
-        { id: 0, brand: "Bmwwwww" },
-        { id: 1, brand: "Mercedes" },
-        { id: 2, brand: "Opel" },
-    ]
-};
-
-
-// app.get("/api", (req, res) => res.send("Jussi's api"));
-// app.get("/api/cars", (req, res) => res.json(myCars));
-// app.get("/api/cars/:id", (req, res) => res.json(myCars.cars.find(car => car.id == req.params.id)));
-
-// app.get("/cars/:id/:brand", (req, res) => res.json(myCars.cars.find(car => car.id == req.params.id && car.brand == req.params.brand)));
 
 app.get("/api/cars", (req, res) => {
     Car.find((err, results) => {
@@ -53,17 +37,13 @@ app.get("/api/cars/:id", (req, res) => {
 });
 
 app.post("/api/cars", (req, res) => {
-    // console.log(req.body);
-
     let car = new Car({
-        id: myCars.cars.length,
         ...req.body                 //This takes and pulls apart an iterable object and regelt die torrie vanzelf.
     });
 
-    // myCars.cars.push(car);
     car.save((err, newCar) => {
         console.log("Saved!");
-        res.json({status: "succes", car: newCar});
+        res.json({ status: "succes", car: newCar });
     });
 
     res.json({
